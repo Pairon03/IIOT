1969ecf4633c7fda1ecf94ce9af7b8835a89cb6a
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-02-24 20:29:26 -0300

    Not found

--- IIOT/DOMOTICA/domotica/src/views/Management.vue
+++ IIOT/DOMOTICA/domotica/src/views/Management.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-
+    <h1>Hello Management!</h1>
 </template>
 
 <style scoped lang="scss">
---
+++ IIOT/DOMOTICA/domotica/src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Not Found! 😭</h1>
+    <img src="https://th.bing.com/th/id/R.4f4f70013b6600d9b68703d8c49f875e?rik=NPkPJwyMtAWYcg&riu=http%3a%2f%2fi1.kym-cdn.com%2fphotos%2fimages%2foriginal%2f000%2f595%2f451%2f99c.png&ehk=GSHEp3moNZ09bO%2fD4LYxBhpSS6lIi8tVCeUmdprmr4k%3d&risl=&pid=ImgRaw&r=0" alt="">
+</template>
+
+
+<style scoped lang="scss">
+
+</style>
