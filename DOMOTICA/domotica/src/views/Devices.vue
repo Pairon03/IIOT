2e1f1c72c0a5e6071d75eda97809467dd80c8662
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-03-10 20:57:29 -0300

    Botões ON/OFF
    Botões mudam de cores dependendo do estado do device, ligado ou desligado.

--- DOMOTICA/domotica/src/views/Devices.vue
+++ DOMOTICA/domotica/src/views/Devices.vue
@@ -1,15 +1,53 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-   
+import { Device } from '@/models/devices';
+import { ref, reactive } from 'vue';
+
+const ar: Device = reactive(new Device());
+ar.name = 'Ar condicionado Samsung';
+ar.state = true;
+ar.icon = 'mode_cool';
+
+const tv: Device = reactive(new Device());
+tv.name = 'Smart TV LG';
+tv.icon = 'tv';
+
+const iluminacao: Device = reactive(new Device());
+iluminacao.name = 'Lâmpada Led';
+iluminacao.state = true;
+iluminacao.icon = 'light';
+
 
 </script>
 
 <template>
-    <h1>Hello Devices!</h1>
-   
-
+    <h1>Seus Dispositivos: </h1>
+    
+    <div>        
+        <h5>{{ ar.name }}</h5>
+        <span class="icons material-icons-round">{{ ar.icon }}</span>
+        <div :class="`button-${ar.state}`">
+            <button class="on">ON</button>
+            <button class="off">OFF</button>
+        </div>      
+    </div>
+    
 </template>
 
 <style scoped lang="scss">
-    
+    .button-true{
+        .on{
+            background-color: green;
+        }
+        .off{
+            background-color: white;
+        }
+    }
+    .button-false{
+        .on{
+            background-color: white;
+        }
+        .off{
+            background-color: red;
+        }
+    }
 </style>
