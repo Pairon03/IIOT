c18457f8c3a8cc575d8121225d1c6b9cbfc74ab6
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-03-10 19:23:54 -0300

    Biblioteca de icons
    npm material icons

--- DOMOTICA/domotica/src/models/devices.ts
+++ DOMOTICA/domotica/src/models/devices.ts
@@ -1,10 +1,21 @@
 
 export class Device {
     name: string = '';
-    value: boolean = false;
+    state: boolean = false;
+    icon: string = ''
 }
 
 export class Environment {
     name: string = '';
     devices: Array<Device> = [];    
 }
+
+const lampada = new Device();
+lampada.name = 'led mesa escritorio';
+
+const ventilador = new Device();
+ventilador.name = 'ventilador de teto';
+
+const quarto = new Environment();
+quarto.name = 'quarto';
+quarto.devices = [lampada,ventilador];
