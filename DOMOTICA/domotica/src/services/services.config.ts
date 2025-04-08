26ab7a4809f7c6c5498924fee7563de985674fa1
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2025-04-07 22:11:37 -0300

    atualizando axios

--- DOMOTICA/domotica/src/services/services.config.ts
+++ DOMOTICA/domotica/src/services/services.config.ts
@@ -1,9 +1,20 @@
-import axios, {type AxiosResponse} from "axios"
+import axios, {AxiosHeaders, type AxiosResponse} from "axios";
+export const BASE_URL = import.meta.env.CONTENTFUL_BASE_ENDPOINT;
 
 
+export const getAxios = (target:'cdn'|'api', token:String, headers: AxiosHeaders)=> {
+    headers.set('Authorization', `Bearer ${token}`);
+    headers.set('Content-Type', 'application/json');
 
-export const getAxios = ()=> {
     const createdAxios = axios.create({
-        baseURL: 
+        baseURL: `https://${target}${BASE_URL}`,
+        timeout: 4000,
+        headers: headers
     });
+    createdAxios.interceptors.response.use(getAxiosResponse);
+    return createdAxios;
+}
+
+const getAxiosResponse = (response:AxiosResponse)=> {
+    return response.data;
 }
